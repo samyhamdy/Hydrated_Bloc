b397e43d739173a6470672371a9cf5a3968eaf2f
samy hamdy <samyhamdy221152@gmail.com>
2024-09-15 00:45:36 +0300

    Hydrated Bloc Explaination

---
+++ hydrated _bloc_tutorial/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'hydrated_bloc_example.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getTemporaryDirectory(),
+  );
+  runApp(const App());
+}
